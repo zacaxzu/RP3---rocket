78deeb7e80e5eee6de6eb7e2a1ba62f6350bef39
zacaxzu <zeljka.baca1@gmail.com>
2024-01-10 13:52:56 +0100

    First upload

---
+++ Raketa1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Raketa1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.brod = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelaBodovi = new System.Windows.Forms.Label();
+            this.prepreka1 = new System.Windows.Forms.PictureBox();
+            this.prepreka2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.brod)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prepreka1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prepreka2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // brod
+            // 
+            this.brod.BackColor = System.Drawing.Color.Transparent;
+            this.brod.Image = global::Raketa1.Properties.Resources.brod;
+            this.brod.Location = new System.Drawing.Point(214, 407);
+            this.brod.Name = "brod";
+            this.brod.Size = new System.Drawing.Size(75, 134);
+            this.brod.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.brod.TabIndex = 0;
+            this.brod.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelaBodovi
+            // 
+            this.labelaBodovi.AutoSize = true;
+            this.labelaBodovi.BackColor = System.Drawing.Color.Transparent;
+            this.labelaBodovi.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelaBodovi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelaBodovi.Location = new System.Drawing.Point(12, 9);
+            this.labelaBodovi.Name = "labelaBodovi";
+            this.labelaBodovi.Size = new System.Drawing.Size(159, 39);
+            this.labelaBodovi.TabIndex = 1;
+            this.labelaBodovi.Text = "Bodovi: 0";
+            // 
+            // prepreka1
+            // 
+            this.prepreka1.Image = global::Raketa1.Properties.Resources.prepreka;
+            this.prepreka1.Location = new System.Drawing.Point(12, 261);
+            this.prepreka1.Name = "prepreka1";
+            this.prepreka1.Size = new System.Drawing.Size(249, 50);
+            this.prepreka1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.prepreka1.TabIndex = 2;
+            this.prepreka1.TabStop = false;
+            // 
+            // prepreka2
+            // 
+            this.prepreka2.Image = global::Raketa1.Properties.Resources.prepreka;
+            this.prepreka2.Location = new System.Drawing.Point(271, 12);
+            this.prepreka2.Name = "prepreka2";
+            this.prepreka2.Size = new System.Drawing.Size(249, 50);
+            this.prepreka2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.prepreka2.TabIndex = 3;
+            this.prepreka2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(532, 553);
+            this.Controls.Add(this.prepreka2);
+            this.Controls.Add(this.prepreka1);
+            this.Controls.Add(this.labelaBodovi);
+            this.Controls.Add(this.brod);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Raketa";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.brod)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prepreka1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.prepreka2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox brod;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelaBodovi;
+        private System.Windows.Forms.PictureBox prepreka1;
+        private System.Windows.Forms.PictureBox prepreka2;
+    }
+}
+
---
+++ Raketa1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raketa1
+{
+    public partial class Form1 : Form
+    {
+        float sirina, visina;
+        public Form1()
+        {
+            InitializeComponent();
+            sirina = ClientSize.Width;
+            visina = ClientSize.Height;
+            PocetnePostavke();
+            timer1.Start();
+            DoubleBuffered = true;
+        }
+        private void povecajBodove(int dobiveniBodovi)
+        {
+            bodovi += dobiveniBodovi;
+            labelaBodovi.Text = "Bodovi: " + bodovi;
+        }
+        private void PocetnePostavke()
+        {
+            bodovi = 0;
+            povecajBodove(0);
+            lijevo = desno = false;
+            kretanje = false;
+            brzinaPozadine = 0.5f;
+            brzinaZida = 4;
+            brzinaBroda = 5;
+            koordPozadina = new float[] { -visina, 0 };
+            koordZid = new float[] { -visina, 0 };
+            brod.Location = new Point(
+                (int)sirina / 2 - brod.Size.Width / 2,
+                (int)visina - brod.Size.Height - 10
+            );
+        }
+
+        Image pozadina = Properties.Resources.pozadina;
+        Image zid = Properties.Resources.zid;
+        float brzinaPozadine, brzinaZida, brzinaBroda;
+        float[] koordPozadina, koordZid;
+        bool kretanje, lijevo, desno;
+        int bodovi;
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up && kretanje)
+                kretanje = false;
+            if (e.KeyCode == Keys.Left)
+                lijevo = false;
+            if (e.KeyCode == Keys.Right)
+                desno = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up && !kretanje)
+                kretanje = true;
+            if (e.KeyCode == Keys.Left)
+                lijevo = true;
+            if (e.KeyCode == Keys.Right)
+                desno = true;
+        }
+
+        private void PomakniPozadinu()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                if (koordPozadina[i] > visina)
+                    koordPozadina[i] -= 2 * visina;
+                if (koordZid[i] > visina)
+                    koordZid[i] -= 2 * visina;
+                if (kretanje == true)
+                {
+                    koordPozadina[i] += brzinaPozadine;
+                    koordZid[i] += brzinaZida;
+                }
+            }
+            Invalidate();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            PomakniPozadinu();
+            if (lijevo && !desno &&
+                brod.Left - brzinaBroda >= 0.1 * sirina)
+                brod.Left -= (int)brzinaBroda;
+            if (desno && !lijevo &&
+                brod.Right + brzinaBroda <= 0.9 * sirina)
+                brod.Left += (int)brzinaBroda;
+            if (kretanje)
+            {
+                prepreka1.Top += (int)brzinaZida;
+                prepreka2.Top += (int)brzinaZida;
+                if (prepreka1.Top > visina)
+                {
+                    povecajBodove(1);
+                    prepreka1.Top = -prepreka1.Height;
+                }
+                if (prepreka2.Top > visina)
+                {
+                    povecajBodove(1);
+                    prepreka2.Top = -prepreka2.Height;
+                }
+            }
+            Invalidate();
+            if (brod.Bounds.IntersectsWith(prepreka1.Bounds)
+                || brod.Bounds.IntersectsWith(prepreka2.Bounds))
+                GameOver();
+        }
+
+        private void GameOver()
+        {
+            timer1.Stop();
+            MessageBox.Show("Osvojeni bodovi: "
+                + bodovi, "Igra je završila!");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 2; ++i)
+            {
+                e.Graphics.DrawImage(pozadina, 0, koordPozadina[i], sirina, visina);
+            }
+            for (int i = 0; i < 2; ++i)
+            {
+                e.Graphics.DrawImage(zid, 0, koordZid[i], 0.1f * sirina, visina);
+                e.Graphics.DrawImage(zid, 0.9f * sirina, koordZid[i],
+                    0.1f * sirina, visina);
+            }
+        }
+    }
+}
